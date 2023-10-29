377401438f343ba2edafa0ba29d52d56c4841304
linsyhen99 <linsyhen99@gmail.com>
2023-10-29 07:43:33 +0200

    Udpated env example and support retrieving transaction through rpc instead of a hard coded string[]
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- examples/ek-event-parser/src/helpers/loadEnvironmentVariables.ts
+++ examples/ek-event-parser/src/helpers/loadEnvironmentVariables.ts
@@ -4,14 +4,24 @@ config();
 
 export type Environment = {
   programHash: string;
+  transactionHash: string;
+  rpc_url: string;
 };
 
 export const getEnvironmentVariables = (): Environment => {
   if (!process.env.PROGRAM_HASH) {
     throw new Error("PROGRAM_HASH environment variable is not set");
   }
+  if (!process.env.TRANSACTION_HASH) {
+    throw new Error("TRANSACTION_HASH environment variable is not set");
+  }
+  if (!process.env.RPC_URL) {
+    throw new Error("RPC_URL environment variable is not set");
+  }
 
   return {
     programHash: process.env.PROGRAM_HASH,
+    transactionHash: process.env.TRANSACTION_HASH,
+    rpc_url: process.env.RPC_URL,
   };
 };
